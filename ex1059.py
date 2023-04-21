4bcc62c5ec117f782f938e3a7cdf176b45294299
rentsoo <renato.msg4@gmail.com>
2023-04-21 17:25:55 -0300

    algoritimos-python-exdiscord

---
+++ ex1059.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    for c in range(1, 101):
+        if c % 2 == 0:
+            print(c)
---
+++ ex1067.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    valor = int(input('Digite um valor: '))
+    for i in range(1, valor + 1, 2):
+        print(i)
